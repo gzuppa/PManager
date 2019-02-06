fa96818492c82bc03e46fa770d018260911ad022
gzuppa <40968747+gzuppa@users.noreply.github.com>
2019-02-06 15:27:52 -0600

    Update router.js

--- client/src/router.js
+++ client/src/router.js
@@ -1,25 +1,35 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Signup from './views/Signup.vue'
+import Login from './views/Login.vue'
 
 Vue.use(Router)
-
-export default new Router({
+const router = new Router({
   mode: 'history',
-  base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
+      meta: { title: 'enamel' }
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+      path: '/signup/:id',
+      name: 'signup',
+      component: Signup,
+      meta: { title: 'Signup - enamel' }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+      meta: { title: 'Login - enamel' }
+     }
   ]
 })
+
+router.afterEach((to, from) => {
+  document.title = to.meta.title
+})
+export default router
