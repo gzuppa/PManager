931edec1a9ca4f838a5417fde45383d067c5f4c3
gzuppa <40968747+gzuppa@users.noreply.github.com>
2019-02-06 16:37:40 -0600

    Update router.js

--- client/src/router.js
+++ client/src/router.js
@@ -3,8 +3,46 @@ import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Signup from './views/Signup.vue'
 import Login from './views/Login.vue'
+import Workspace from './views/Workspace.vue'
+import Folder from './views/Folder.vue'
+import FolderDetail from './views/FolderDetail.vue'
 
 Vue.use(Router)
+
+const login = {
+  path: '/login',
+  name: 'login',
+  component: Login,
+  meta: { title: 'Login - enamel' }
+}
+
+const workspace = {
+  path: '/w',
+  name: 'workspace',
+  component: Workspace,
+  meta: { title: 'Workspace - enamel', requiresAuth: true },
+  children: [
+    {
+      path: 'folder/:id',
+      component: Folder,
+      props: true,
+      children: [
+        {
+          path: '',
+          name: 'folder',
+          component: FolderDetail
+        },
+        // {
+        //   path: 'task/:taskId',
+        //   name: 'task',
+        //   component: Task,
+        //   props: true
+        // }
+      ]
+    }
+  ]
+}
+
 const router = new Router({
   mode: 'history',
   routes: [
@@ -12,7 +50,7 @@ const router = new Router({
       path: '/',
       name: 'home',
       component: Home,
-      meta: { title: 'enamel' }
+      meta: { title: 'enamel', redirect: true}
     },
     {
       path: '/signup/:id',
@@ -20,16 +58,27 @@ const router = new Router({
       component: Signup,
       meta: { title: 'Signup - enamel' }
     },
-    {
-      path: '/login',
-      name: 'login',
-      component: Login,
-      meta: { title: 'Login - enamel' }
-     }
+    login,
+    workspace
   ]
+})
+
+router.beforeEach((to, from, next) => {
+  const auth = localStorage.getItem('user-id')
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if(!auth) {
+      next(login)
+    }
+  } else if (to.matched.some(record => record.meta.redirect)) {
+    if(auth) {
+      next(workspace)
+    }
+  }
+  next()
 })
 
 router.afterEach((to, from) => {
   document.title = to.meta.title
 })
+
 export default router
