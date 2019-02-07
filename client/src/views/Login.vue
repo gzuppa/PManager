dc07853944527e8af7b9a47d102b9497ada89b90
gzuppa <40968747+gzuppa@users.noreply.github.com>
2019-02-07 10:54:46 -0600

    Update Login.vue

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,15 +1,18 @@
 <template>
   <el-container>
     <el-header>
+      <navigation></navigation>
     </el-header>
-<el-main>
+
+    <el-main>
       <div class="container-center">
         <h2>Log in</h2>
         
         <div v-if="error" class="error">
           {{ error }}
         </div>
-<el-form ref="form" :model="form">
+
+        <el-form ref="form" :model="form">
           <el-form-item>
             <label>Email</label>
             <el-input v-model="form.email" placeholder="Email"></el-input>
@@ -17,17 +20,21 @@
             <el-input v-model="form.password" type="password" placeholder="Password"></el-input>
           </el-form-item>
           <el-form-item>
-            <el-button type="primary" @click.once="login">Log in</el-button>
+            <el-button type="primary" @click="login">Log in</el-button>
           </el-form-item>
         </el-form>
-<div>
+
+        <div>
           <span>Don't have an account?</span>
           <router-link :to="{name: 'home'}" class="link">Create an account</router-link>
         </div>
       </div>
-</el-main>
+
+    </el-main>
   </el-container>
+
 </template>
+
 <script>
 import { Login } from '../constants/query.gql'
 export default {
@@ -42,25 +49,25 @@ export default {
   },
   methods: {
     async login() {
-this.$apollo.provider.clients.defaultClient.cache.reset()
-
+      this.$apollo.provider.clients.defaultClient.cache.reset()
+      // const validated = await this.$validator.validate()
       const { email, password } = this.form
-    if (email && password) {
-      this.$apollo.mutate({
-        mutation: Login,
-        variables: { email, password }
-      }).then(async (data) => {
-        const login = data.data.login
-        const id = login.user.id
-        const token = login.token
-        this.saveUserData(id, token)
-        this.$router.push({name: 'workspace'})
-      }).catch((error) => {
-        this.error = 'Invalid email or password'
-        console.log(error)
-      })
-    }
-  },
+      if (email && password) {
+        this.$apollo.mutate({
+          mutation: Login,
+          variables: { email, password }
+        }).then(async (data) => {
+          const login = data.data.login
+          const id = login.user.id
+          const token = login.token
+          this.saveUserData(id, token)
+          this.$router.push({name: 'workspace'})
+        }).catch((error) => {
+          this.error = 'Invalid email or password'
+          console.log(error)
+        })
+      }
+    },
     saveUserData (id, token) {
       localStorage.setItem('user-id', id)
       localStorage.setItem('user-token', token)
@@ -69,6 +76,7 @@ this.$apollo.provider.clients.defaultClient.cache.reset()
   }
 }
 </script>
+
 <style scoped>
 .el-button {
   width: 100%;
