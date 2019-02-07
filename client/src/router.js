12f2fb8bada420debcbc9ebf1662c1698926df54
gzuppa <40968747+gzuppa@users.noreply.github.com>
2019-02-07 15:02:38 -0600

    Update router.js

--- client/src/router.js
+++ client/src/router.js
@@ -1,11 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import store from './store'
 import Home from './views/Home.vue'
 import Signup from './views/Signup.vue'
 import Login from './views/Login.vue'
 import Workspace from './views/Workspace.vue'
 import Folder from './views/Folder.vue'
+import Workload from './views/Workload.vue'
+import List from './views/List.vue'
 import FolderDetail from './views/FolderDetail.vue'
+import Task from './views/Task.vue'
+import Account from './views/Account.vue'
+import Decline from './views/Decline.vue'
+import Notifications from './views/Notifications.vue'
+import Settings from './views/Settings.vue'
+import Timelog from './views/Timelog.vue'
 
 Vue.use(Router)
 
@@ -28,17 +37,53 @@ const workspace = {
       props: true,
       children: [
         {
-          path: '',
-          name: 'folder',
-          component: FolderDetail
+          path: 'list',
+          component: List,
+          props: true,
+          children: [
+            {
+              path: '',
+              name: 'folder',
+              props: true,
+              component: FolderDetail
+            },
+            {
+              path: ':taskId',
+              name: 'task',
+              component: Task,
+              props: true
+            }
+          ]
         },
-        // {
-        //   path: 'task/:taskId',
-        //   name: 'task',
-        //   component: Task,
-        //   props: true
-        // }
+        {
+          path: 'workload',
+          name: 'workload',
+          component: Workload,
+          props: true
+        }
       ]
+    },
+    {
+      path: 'notifications',
+      name: 'notifications',
+      component: Notifications,
+      meta: { title: 'Notifications - enamel' },
+      props: true,
+      children: [
+        {
+          path: ':id/:taskId',
+          name: 'notification-task',
+          component: Task,
+          props: true
+        }
+      ]
+    },
+    {
+      path: 'timelog/:id/:year/:month',
+      name: 'timelog',
+      component: Timelog,
+      meta: { title: 'Timelog - enamel' },
+      props: true,
     }
   ]
 }
@@ -50,7 +95,7 @@ const router = new Router({
       path: '/',
       name: 'home',
       component: Home,
-      meta: { title: 'enamel', redirect: true}
+      meta: { title: 'enamel', redirect: true }
     },
     {
       path: '/signup/:id',
@@ -59,7 +104,25 @@ const router = new Router({
       meta: { title: 'Signup - enamel' }
     },
     login,
-    workspace
+    {
+      path: '/decline/:id',
+      name: 'decline',
+      component: Decline,
+      meta: { title: 'enamel' }
+    },
+    workspace,
+    {
+      path: '/account',
+      name: 'account',
+      component: Account,
+      meta: { title: 'Accounts - enamel', requiresAuth: true }
+    },
+    {
+      path: '/settings',
+      name: 'settings',
+      component: Settings,
+      meta: { title: 'Settings - enamel', requiresAuth: true }
+    }
   ]
 })
 
@@ -78,7 +141,7 @@ router.beforeEach((to, from, next) => {
 })
 
 router.afterEach((to, from) => {
-  document.title = to.meta.title
+  document.title = `${store.state.notification ? '*' : ''} ${to.meta.title}`
 })
 
 export default router
