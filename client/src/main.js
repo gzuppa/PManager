8284986bf6022b560f7f41937dee29b3564d464c
gzuppa <40968747+gzuppa@users.noreply.github.com>
2019-02-07 15:02:42 -0600

    Update main.js

--- client/src/main.js
+++ client/src/main.js
@@ -1,31 +1,80 @@
-import Vue from 'vue' 
+import Vue from 'vue'
+// import VeeValidate from 'vee-validate'
 import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { persistCache } from 'apollo-cache-persist'
 import { onError } from "apollo-link-error"
-import { ApolloLink } from 'apollo-link'
+import { ApolloLink, split } from 'apollo-link'
 import { HttpLink } from 'apollo-link-http'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
 import { enableExperimentalFragmentVariables } from 'graphql-tag'
 import VueApollo from 'vue-apollo'
 import ElementUI from 'element-ui'
+import VueAnalytics from 'vue-analytics'
+import draggable from 'vuedraggable'
+import linkify from 'vue-linkify'
+
+Vue.directive('linkified', linkify)
 
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// import './registerServiceWorker'
 
 import 'element-ui/lib/theme-chalk/index.css'
 import './assets/css/style.scss'
 
+import Avatar from '@/components/icons/Avatar.vue'
+import CloseButton from '@/components/icons/CloseButton.vue'
+import RemoveButton from '@/components/icons/RemoveButton.vue'
 import PlusButton from '@/components/icons/PlusButton.vue'
+import Navigation from '@/components/Navigation'
+
+Vue.component('draggable', draggable)
+Vue.component('navigation', Navigation)
+Vue.component('avatar', Avatar)
+Vue.component('close-button', CloseButton)
+Vue.component('remove-button', RemoveButton)
 Vue.component('plus-button', PlusButton)
 
-Vue.use(ElementUI)
+Vue.use(VueAnalytics, {
+  id: 'UA-62716182-7'
+})
 
+Vue.use(ElementUI)
+// Vue.use(VeeValidate)
 Vue.config.productionTip = false
 
 const uri = `${process.env.VUE_APP_URI}/graphql`
-const httpLink = new HttpLink({uri})
+const httpLink = new HttpLink({
+  uri,
+})
 
-const cache = new InMemoryCache({})
+// const wsLink = new WebSocketLink({
+//   uri: `ws://${process.env.VUE_APP_URI}/subscriptions`,
+//   options: {
+//     reconnect: true,
+//   },
+// })
+
+const cache = new InMemoryCache({
+  cacheRedirects: {
+    Query: {
+      getFolder: (_, args, { getCacheKey }) => {
+        return getCacheKey({ __typename: 'Folder', id: args.id })
+      },
+      getTask: (_, args, { getCacheKey }) => {
+        return getCacheKey({ __typename: 'Task', id: args.id })
+      }
+    },
+  },
+})
+
+// persistCache({
+//   cache,
+//   storage: window.localStorage,
+// })
 
 const authMiddleware = new ApolloLink((operation, forward) => {
   const token = localStorage.getItem('user-token')
@@ -46,6 +95,21 @@ const errorLink = onError(({ graphQLErrors, networkError }) => {
     )
   if (networkError) console.log(`[Network error]: ${networkError}`)
 })
+
+// const link = split(
+//   // split based on operation type
+//   ({ query }) => {
+//     const { kind, operation } = getMainDefinition(query)
+//     return kind === 'OperationDefinition' &&
+//       operation === 'subscription'
+//   },
+//   wsLink,
+//   ApolloLink.from([
+//     errorLink,
+//     authMiddleware,
+//     httpLink
+//   ])
+// )
 
 const client = new ApolloClient({
   link: ApolloLink.from([
