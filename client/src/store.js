a2b03cfd6048c8d780ac0c1b7fba7dd5471aad48
gzuppa <40968747+gzuppa@users.noreply.github.com>
2019-02-07 15:02:36 -0600

    Update store.js

--- client/src/store.js
+++ client/src/store.js
@@ -7,10 +7,38 @@ export default new Vuex.Store({
   state: {
     userId: localStorage.getItem('user-id'),
     activeWidget: null,
+    openState: {},
+    mode: 'default',
+    tempItem: null,
+    notification: false,
+    filterAll: []
   },
   mutations: {
     changeActiveWidget(state, key) {
       state.activeWidget = state.activeWidget === key ? null : key
+    },
+    changeOpenState(state, {id, val}) {
+      const open = state.openState[id]
+      state.openState[id] = val || (open === undefined ? true : !open)
+    },
+    changeMode(state, {type, item}) {
+      state.mode = type
+      state.tempItem = item
+      state.activeWidget = null
+    },
+    setNotification(state, val) {
+      state.notification = val
+    },
+    addToFilter(state, id) {
+      state.filterAll.push(id)
+    },
+    removeFromFilter(state, id) {
+      state.filterAll = state.filterAll.filter(o => o !== id)
+    }
+  },
+  actions: {
+    changeActiveWidget({state, commit}, key) {
+      commit('changeActiveWidget', key)
     }
   }
 })
